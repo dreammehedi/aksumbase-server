9e3878745b47558f03f1fda37b941e5bcb112813
dreammehedi <dreammehedihassan@gmail.com>
2025-06-21 11:42:07 +0600

    update

--- utils/createUserRoleAndTransaction.js
+++ utils/createUserRoleAndTransaction.js
@@ -1,4 +1,4 @@
-import prisma from "../lib/prisma";
+import prisma from "../lib/prisma.js";
 
 // utils/createUserRoleAndTransaction.js
 export const createUserRoleAndTransaction = async (session) => {
