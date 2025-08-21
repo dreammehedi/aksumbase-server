3b443e1dc07e16cf4b2e2745b2f6aca556affa0e
dreammehedi <dreammehedihassan@gmail.com>
2025-08-21 14:54:21 +0600

    update server

--- helper/sendEmail.js
+++ helper/sendEmail.js
@@ -20,9 +20,9 @@ export const sendEmail = async ({ to, subject, html }) => {
       user: emailConfig.emailAddress,
       pass: decryptedPassword,
     },
-     tls: {
-    rejectUnauthorized: false, // 👉 SSL মিসম্যাচ ইগনোর করবে
-  },
+    tls: {
+      rejectUnauthorized: false, // 👉 SSL মিসম্যাচ ইগনোর করবে
+    },
   });
 
   const mailOptions = {
@@ -34,39 +34,3 @@ export const sendEmail = async ({ to, subject, html }) => {
 
   await transporter.sendMail(mailOptions);
 };
-
-// import nodemailer from "nodemailer";
-// import prisma from "../lib/prisma.js";
-// import decrypt from "./decrypt.js";
-
-// export const sendEmail = async ({ to, subject, html }) => {
-//   const emailConfig = await prisma.emailConfiguration.findFirst();
-
-//   if (!emailConfig) {
-//     throw new Error("Email configuration not found.");
-//   }
-
-//   const decryptedPassword = decrypt(emailConfig.emailPassword);
-//   if (!decryptedPassword) {
-//     throw new Error("Failed to decrypt email password");
-//   }
-
-//   const transporter = nodemailer.createTransport({
-//     host: emailConfig.emailHost,
-//     port: emailConfig.emailPort,
-//     secure: emailConfig.emailEncryption === "ssl",
-//     auth: {
-//       user: emailConfig.emailAddress,
-//       pass: decryptedPassword,
-//     },
-//   });
-
-//   const mailOptions = {
-//     from: `"${emailConfig.emailFromName}" <${emailConfig.emailAddress}>`,
-//     to,
-//     subject,
-//     html,
-//   };
-
-//   await transporter.sendMail(mailOptions);
-// };
